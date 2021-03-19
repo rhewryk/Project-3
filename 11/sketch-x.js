3c3986bfdcd886f11bfecbae9c0ea9d413573ba2
rhewryk <78235971+rhewryk@users.noreply.github.com>
2021-03-19 11:07:00 -0400

    Update sketch-x.js

--- 11/sketch-x.js
+++ 11/sketch-x.js
@@ -1,15 +1,17 @@
 function setup() {
 	var c = createCanvas(1000,1000); // width and height in pixels
+background(255);
 
-scale(11.5);
+
+scale(8.5);
 noFill();
-stroke(255, 102, 0);
+stroke(255, 0, 0);
 bezier(85, 20, 10, 10, 90, 90, 15, 80);
 
 
 noFill();
-stroke(255, 102, 0);
-bezier(0, 20, 65, 10, 10, 90, 85, 80);
+stroke(255, 0, 0);
+bezier(15, 20, 90, 10, 10, 90, 85, 80);
 
 
 
