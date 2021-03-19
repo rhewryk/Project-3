6f90b2ff4967a96875c51b5bbd5bdbd75fd414ce
rhewryk <78235971+rhewryk@users.noreply.github.com>
2021-03-19 11:02:25 -0400

    Update sketch-x.js

--- 10/sketch-x.js
+++ 10/sketch-x.js
@@ -1,34 +1,18 @@
-const sketchNumber = "10";
-const sWidth = 1000;
-const sHeight = 1000;
-
-let a = 0.0;
-let aVelocity = 0.0;
-let aAcceleration = 0.1
+var font;
 
 function preload() {
 	font = loadFont('PERTILI.TTF')
 }
 
 function setup() {
-	createCanvas(sWidth,sHeight);
+	createCanvas(1000,1000);
+	textFont(font);
+	textSize(40);
+	text('X', 100, 100);
 }
 
 
 
-
-function draw() {
-	a+= aVelocity;
-	aVelocity +=aAcceleration;
-
-	aAcceleration = map(mouseX,0,width,-0.001, 0.001)
-	background(255)
-	stroke(0)
-	fill(127)
-	translate(width/2,height/2)
-	rotate(a)
-	rect(0,0,64,36)
-}
 
 
 
